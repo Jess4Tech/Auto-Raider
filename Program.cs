e836fbd7edacc1f41f4530220e100c807bc5377a
Jess <mcroolz4ever@gmail.com>
2019-10-06 15:09:42 -0400

    Add Project

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using WindowsInput.Native;
+using WindowsInput;
+
+
+namespace AutoRaid
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            InputSimulator simulator = new InputSimulator();
+            Console.WriteLine("What message do you want to spam?");
+            string spamLine = Console.ReadLine();
+            static void openPrivate()
+            {
+                string privateBrowser = "/C start microsoft-edge:http://www.discordapp.com/";
+                System.Diagnostics.Process.Start("CMD.exe", privateBrowser);
+            }
+            openPrivate();
+            Console.ReadKey();
+            while (true)
+            {
+                try
+                {
+                    simulator.Keyboard.TextEntry(spamLine).KeyPress(VirtualKeyCode.RETURN).Sleep(1000);
+                } catch (System.Exception)
+                {
+                    break;
+                }
+            }
+            
+        }
+    }
+}
